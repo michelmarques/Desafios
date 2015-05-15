12556ff3d0abb989a021237a3af15db779f00c1c
michelmarques <michel.marques.me@gmail.com>
2015-05-14 22:19:11 -0300

    Criado método getPrecedente

--- calculadora/src/calculadora/Calc.java
+++ calculadora/src/calculadora/Calc.java
@@ -1,6 +1,33 @@
 package calculadora;
 
 public class Calc {
+    private int getPrecedente(String formula) {
+	int precedente = 0;
+	int posicao = 0;
+	while (posicao < formula.length()) {
+	    if (formula.charAt(posicao) == '(') {
+		posicao++;
+		while ((formula.charAt(posicao) != ')') && (posicao < formula.length())) {
+		    posicao++;
+		}
+	    
+		if (posicao >= formula.length() -1) {
+		    precedente = -1; 
+		    break;
+		}
+	    }
+	    else if ((formula.charAt(posicao) == '+') || formula.charAt(posicao) == '-') {
+		 precedente = posicao;
+		 //break;
+	    }
+	    else if (((formula.charAt(posicao) == '*') || (formula.charAt(posicao) == '/')) && (precedente == 0)) {
+		precedente = posicao;
+	    }
+	    posicao++;
+	}
+	return precedente;
+    }
+    
     public double calcular(String formula) {
 	double valor = 0;
 	String parte1, parte2;
@@ -22,34 +49,25 @@ public class Calc {
 	    System.out.println("Erro na contagem dos parenteses.");
 	}
 	else {
-	    int posicao = 0;
-	    while ((valor == 0) && (posicao < formula.length())) {
-                if (formula.charAt(posicao) == '(') {
-		    posicao++;
-		    while ((formula.charAt(posicao) != ')') && (posicao < formula.length())) {
-                        posicao++;
-		    }
-		    
-		    if (posicao >= formula.length() -1) {
-                        formula = formula.substring(1, formula.length() -1);
-                        posicao = 0; // voltar ao início com a formula sem os parênteses inicial e final
-		    }
-                }
-		else if ((formula.charAt(posicao) == '+') || formula.charAt(posicao) == '-') {
-		     precedente = posicao;
-		     break;
-                }
-		else if ((formula.charAt(posicao) == '*') || formula.charAt(posicao) == '/') {
-                    precedente = posicao;
-                }
-                posicao++;
-            }
-	    if (precedente == 0) {
+	    precedente = getPrecedente(formula);
+	    if (precedente <0) {
+		formula = formula.substring(1, formula.length() -1);
+		valor = calcular(formula);
+	    }
+	    else if (precedente == 0) {
 		valor = Double.parseDouble(formula);
 	    }
 	    else {
-		parte1 = formula.substring(0, precedente);
-		parte2 = formula.substring(precedente +1, formula.length());
+		parte1 = formula.substring(0, precedente).trim();
+		parte2 = formula.substring(precedente +1, formula.length()).trim();
+		
+		if (parte1.isEmpty()) {
+		    parte1 = "0";
+		}
+		
+		if (parte2.isEmpty()) {
+		    parte2 = "0";
+		}
 		
 		if (formula.charAt(precedente) == '*') {
 		    valor = calcular(parte1) * calcular(parte2);
